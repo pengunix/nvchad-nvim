fb08e87212fa96ba78ebff5a8ea79a9be9426e51
pengunix <hoshimaemi@outlook.com>
2026-06-29 12:39:13 +0800

    添加cmake python rust LSP, 添加开始页图标

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "rust-analyzer" }
+local servers = { "clangd", "rust_analyzer", "pyright", "cmakelang" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
